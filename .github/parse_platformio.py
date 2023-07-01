8f68aae5e0d4389cc89d6ce2a08d5ca37d839e86
Atticus Russell <atticusrussell@gmail.com>
2023-07-01 18:11:03 -0400

    only pass env name into pio

--- .github/parse_platformio.py
+++ .github/parse_platformio.py
@@ -8,7 +8,6 @@ environments = []
 for section in config.sections():
     if section.startswith("env:"):
         env_name = section.split("env:")[1]
-        build_flags = config[section].get("build_flags", "").replace('\n', ' ')
-        environments.append({"env": env_name, "build_flags": build_flags})
+        environments.append({"env": env_name})
 
 print(json.dumps(environments))
