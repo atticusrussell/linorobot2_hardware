8973b935fa799e7ce026d54bb2f4d8f6d1e5550c
Atticus Russell <atticusrussell@gmail.com>
2023-07-01 17:56:13 -0400

    Remove newlines

--- .github/parse_platformio.py
+++ .github/parse_platformio.py
@@ -8,7 +8,7 @@ environments = []
 for section in config.sections():
     if section.startswith("env:"):
         env_name = section.split("env:")[1]
-        build_flags = config[section].get("build_flags", "")
+        build_flags = config[section].get("build_flags", "").replace('\n', ' ')
         environments.append({"env": env_name, "build_flags": build_flags})
 
 print(json.dumps(environments))
