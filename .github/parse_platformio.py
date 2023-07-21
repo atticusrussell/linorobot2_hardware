406b19d654bcb798290e8514adfd70cbe2612fa1
Atticus Russell <atticusrussell@gmail.com>
2023-07-21 14:43:24 -0400

    get ROS distros from a list

--- .github/parse_platformio.py
+++ .github/parse_platformio.py
@@ -2,10 +2,27 @@ import configparser
 import json
 import sys
 
-""" This script parses the platformio.ini file and outputs a JSON array of environments to be used by GitHub Actions."""
+"""
+This script parses the platformio.ini file and outputs a JSON array of environments to be used by GitHub Actions.
+
+Usage:
+    python parse_platformio.py <ros_distro1,ros_distro2,...>
+
+Arguments:
+    ros_distro1,ros_distro2,... : A comma-separated list of ROS distributions
+
+Example:
+    python parse_platformio.py rolling,iron,humble,foxy,galactic
+"""
+
+# Check if the script received the correct number of arguments
+if len(sys.argv) != 2:
+    print("Usage: python parse_platformio.py <ros_distro1,ros_distro2,...>")
+    sys.exit(1)
 
 # Get the list of ROS distributions from the command line arguments
-ros_distributions = sys.argv[1:]
+# The input is a comma-separated string, so we split it into a list
+ros_distributions = sys.argv[1].split(',')
 
 # Parse the platformio.ini file
 config = configparser.ConfigParser()
