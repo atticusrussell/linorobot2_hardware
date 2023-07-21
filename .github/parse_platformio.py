5cf56c3d5e3967bdf8b909a479b7924694d41251
Atticus Russell <atticusrussell@gmail.com>
2023-07-21 13:50:26 -0400

    handle ros distros in python script

--- .github/parse_platformio.py
+++ .github/parse_platformio.py
@@ -1,7 +1,11 @@
 import configparser
 import json
+import sys
 
 """ This script parses the platformio.ini file and outputs a JSON array of environments to be used by GitHub Actions."""
+
+# Get the list of ROS distributions from the command line arguments
+ros_distributions = sys.argv[1:]
 
 # Parse the platformio.ini file
 config = configparser.ConfigParser()
@@ -12,7 +16,9 @@ environments = []
 for section in config.sections():
     if section.startswith("env:"):
         env_name = section.split("env:")[1]
-        environments.append({"env": env_name})
+        # Add an entry for each ROS distribution for each environment
+        for ros_distro in ros_distributions:
+            environments.append({"env": env_name, "ros_distro": ros_distro})
 
 # Output the environments as a JSON array
 print(json.dumps(environments))
