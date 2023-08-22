4c222ff98778e494b80afac5ab2e2e21b44c40c2
Atticus Russell <60147768+atticusrussell@users.noreply.github.com>
2023-08-21 23:15:42 -0400

    Enable IMU in config
    added MPU9250 IMU

--- config/lino_base_config.h
+++ config/lino_base_config.h
@@ -32,8 +32,8 @@
 // #define USE_GY85_IMU
 // #define USE_MPU6050_IMU
 // #define USE_MPU9150_IMU
-// #define USE_MPU9250_IMU
-#define USE_FAKE_IMU
+#define USE_MPU9250_IMU
+// #define USE_FAKE_IMU
 
 #define K_P 0.6                             // P constant
 #define K_I 0.8                             // I constant
